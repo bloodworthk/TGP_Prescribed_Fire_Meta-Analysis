6f68d3b268a751ca3f3b7de1fcbe95270397fe8a
Bloodworthk <bloodworthk@si.edu>
2020-06-30 10:14:24 -0400

    adding code to create GUI interface

--- TGP_Prescribed_Fire_Meta_Analysis.R
+++ TGP_Prescribed_Fire_Meta_Analysis.R
@@ -1,22 +1,27 @@
 ##########################################################################################################
 #Project: Prescribed Fire in Tallgrass Priarie Meta-Analysis 
 
-#Contributors: Kathryn Bloodworth, Dirac Twidwall, Alice Boyle, Ellen Welti, Marissa Ahlering, Brian Obermeyer, Chris Helzer, Bob Hamilton, Elizabeth Bach, Clare Kazanski, Sally Koerner, 
+#Contributors: Kathryn Bloodworth, Dirac Twidwall, Alice Boyle, Ellen Welti, Marissa Ahlering, Brian Obermeyer, Chris Helzer, Bob Hamilton, Elizabeth Bach, Clare Kazanski, Sally Koerner 
 
-#Coder: Kathryn Bloodworth
+#Coder: Kathryn Bloodworth, Sarah Gora
 ##########################################################################################################
 
 #### Install and load libraries ####
+#had to first download XQuartz on mac, then followed these steps https://www.andrewheiss.com/blog/2012/04/17/install-r-rstudio-r-commander-windows-osx/, then had to install Rcmdr 
 
-#install.packages("BiocManager")
-library(BiocManager)
+#install.packages("Rcmdr")
+library(Rcmdr)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.11")
 BiocManager::install("EBImage")
-#install.packages("Rcpp")
+install.packages("Rcpp")
 library(Rcpp)
-#install.packages("tcltk2")
+install.packages("tcltk2")
 library(tcltk2)
-#install.packages("metagear")
+install.packages("metagear")
 library(metagear)
+
 
 #### Set Working Directories ####
 
@@ -26,34 +31,30 @@ setwd("/Users/kjbloodw/Box/TNC_TGP_RxFire/Data")
 #Bloodworth - Mac
 setwd("/Users/kathrynbloodworth/Box/TNC_TGP_RxFire/Data")
 
-#### Read in Data frame with first 500 papers ####
-
+#### Read in Data frame with first 10 papers ####
 
 Web_of_Science_FirstPage<-read.csv("Papers/Web_of_Science_06_16_20_Page1.csv")
 
 #### Setting up screening tasks ####
 #Following http://lajeunesse.myweb.usf.edu/metagear/metagear_basic_vignette.html#installation-and-dependencies 
 
-#prime the study-reference dataset - function adds three new columns: Study_ID (unique number for each reference), Reviewers (an empty column with NAs that will populate later with reviewers), Include (will contain the screening efforts by reviewers)
+#prime the study-reference dataset - function adds 4 new columns: Study_ID (unique number for each reference), Reviewers (an empty column with NAs that will populate later with reviewers), 2 columns for Include (will contain the screening efforts by both reviewers)
 
-References_screening_ready<-effort_initialize(Web_of_Science_FirstPage)
+References_screening_ready<-effort_initialize(Web_of_Science_FirstPage,dual = TRUE)
 
 #Create a list with reviewer names to be assigned papers
 Reviewers<-c("Kathryn", "Sarah")
 
 ### Randomly delegate screening efforts to two reviewers (Kathryn and Sarah) ###
-References_unscreened_random <- effort_distribute(References_screening_ready, reviewers = Reviewers)
-
-#check to make sure it worked: display screening tasks
-References_unscreened_random[c("STUDY_ID","REVIEWERS")]
+References_unscreened_random <- effort_distribute(References_screening_ready, dual = TRUE, reviewers = Reviewers, initialize = TRUE, save_split = TRUE)
 
 #From Initialization through assignment of papers (effort distribute) all in one step -- assigns 50/50
-References_unscreened<-effort_distribute(Web_of_Science_FirstPage, reviewers = c("Kathryn", "Sarah"), initialize = TRUE, save_split = TRUE)
+#References_unscreened<-effort_distribute(Web_of_Science_FirstPage, reviewers = c("Kathryn", "Sarah"), initialize = TRUE, save_split = TRUE)
 
 ### Randomly delegating screening efforts with two reviewers (Kathryn and Sarah), where Kathryn takes 80% of studies ### save_splot = TRUE allows this particular split to be saved to a file
 #References_unscreened_60 <- effort_distribute(References_screening_ready, reviewers = Reviewers, effort = c(60,40), save_split = TRUE)
 
-# save files in working directory by name of reviewer
+# show that files are saved in working directory by name of reviewer
 list.files(pattern = "effort")
 
 #Remerge files from both reviewers (do this if reviewers use data frames in the working directory to vet the references -- each reviewer must change the column "Include" to either a YES or NO)
@@ -63,5 +64,4 @@ list.files(pattern = "effort")
 #See how many were vetted yes, no, not vetted. Review progress and check percentage of usable papers
 #References_screened_Summary <- effort_summary(References_screened)
 
-
-abstract_screener("effort_Kathryn.csv", aReviewer = "Kathryn")
+abstract_screener("effort_Kathryn.csv",  aReviewer = "Kathryn")
